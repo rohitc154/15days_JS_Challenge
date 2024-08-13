85fe6d67c07cf957cc61fa557704afebdad59355
rohitc154 <rohitmonu154@gmail.com>
2024-08-13 21:07:04 +0530

    Export file

---
+++ Day13/activityExport.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export const rohit = {
+    nicName: "Monu",
+    qual: "BCA",
+}
+
+export function sub(c, d) {
+    return c - d;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+export default mul;
