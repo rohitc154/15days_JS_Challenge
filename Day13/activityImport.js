aac768e1e21fb576d5a1cb8e34c70695e590e2a8
rohitc154 <rohitmonu154@gmail.com>
2024-08-13 21:07:38 +0530

    Import File

---
+++ Day13/activityImport.js
@@ -0,0 +1,15 @@
+import { add, sub } from "./activityExport.js";
+import { rohit } from "./activityExport.js";
+import mul from "./activityExport.js";
+
+const result = add(2, 3);
+// console.log(result);
+
+rohit.nicName = "Monu Kumar"
+// console.log(rohit);
+
+const resulSub = sub(8, 5)
+// console.log(resulSub);
+
+const resultMul = mul(9, 9)
+// console.log(resultMul);
